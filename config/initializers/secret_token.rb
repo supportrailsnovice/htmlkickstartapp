9fb898ee3167e5d01f88442cddffcf6e5ecf244e
abhic <abhijit.j.chaudhari@gmail.com>
2013-07-08 10:54:46 +0530

    htmlkickstart installed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Htmlkickstartapp::Application.config.secret_token = 'c58c5d7e2fedb7d02352d30e188ce59af8fc89a9610a116c73eee36804a7335e82efb97118005e84fe1548465f35d07c2e4354277ac431c72ce20f8bd9c06064'
